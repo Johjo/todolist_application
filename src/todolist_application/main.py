829df5a2de75614f2b50a271bb7c30d0004d3fce
Jonathan LAURENT <johjo@ytreza.org>
2025-04-01 23:55:54 +0200

    feat: can add and close task

--- src/todolist_application/main.py
+++ src/todolist_application/main.py
@@ -2,6 +2,8 @@ from uuid import uuid4
 
 import streamlit as st
 from todolist_hexagon.fvp.aggregate import FvpSessionSetPort
+from todolist_hexagon.fvp.read.which_task import TodolistPort
+from todolist_hexagon.read_adapter_dependencies import ReadAdapterDependenciesPort
 from todolist_hexagon.shared.type import TodolistKey, TaskKey, TaskName, TodolistName
 from todolist_hexagon.todolist.aggregate import TaskSnapshot
 from todolist_hexagon.todolist.port import TaskKeyGeneratorPort, TodolistSetPort
@@ -10,6 +12,7 @@ from todolist_hexagon.write_adapter_dependencies import WriteAdapterDependencies
 
 from todolist_application.infra.fvp_memory import FvpMemory
 from todolist_application.infra.memory import Memory
+from todolist_application.secondary.fvp.read.which_task.todolist_memory import TodolistInMemory
 from todolist_application.secondary.fvp.write.fvp_session_set_in_memory import FvpSessionSetInMemory
 from todolist_application.secondary.todolist.task_key_generator_random import TaskKeyGeneratorRandom
 from todolist_application.secondary.todolist.todolist_set.todolist_set_in_memory import TodolistSetInMemory
@@ -34,39 +37,75 @@ if "memory" not in st.session_state:
     st.session_state["memory"] = Memory()
 memory = st.session_state["memory"]
 
+if "fvp_memory" not in st.session_state:
+    st.session_state["fvp_memory"] = FvpMemory()
+fvp_memory = st.session_state["fvp_memory"]
+
 if "todolist_key" not in st.session_state:
     st.session_state["todolist_key"] = uuid4()
 todolist_key = st.session_state["todolist_key"]
 
 
-dependencies = WriteAdapterDependencies(memory=memory)
+write_dependencies = WriteAdapterDependencies(memory=memory)
+
+
+class ReadAdapterDependencies(ReadAdapterDependenciesPort):
+    def todolist(self) -> TodolistPort:
+        return TodolistInMemory(memory=memory)
+
+    def fvp_session_set(self) -> FvpSessionSetPort:
+        return FvpSessionSetInMemory(fvp_memory=fvp_memory)
+
+
+read_dependencies = ReadAdapterDependencies()
 
 
 
-create_todolist_use_case = UseCaseDependencies(adapter_dependencies=dependencies).create_todolist()
+create_todolist_use_case = UseCaseDependencies(adapter_dependencies=write_dependencies).create_todolist()
 create_todolist_use_case.execute(todolist_key=todolist_key, todolist_name=TodolistName("test"))
 
 
 def add_task(task_name: str) -> None:
-    open_task = UseCaseDependencies(adapter_dependencies=dependencies).open_task()
+    open_task = UseCaseDependencies(adapter_dependencies=write_dependencies).open_task()
     open_task.execute(todolist_key=todolist_key, task_key=TaskKey(uuid4()), name=TaskName(task_name))
 
 def list_task() -> list[TaskSnapshot]:
     tasks = memory.all_tasks(todolist_key=todolist_key)
-    return tasks
+    return [task for task in tasks if task.is_open]
+
+def close_task(task_key: TaskKey) -> None:
+    use_case = UseCaseDependencies(adapter_dependencies=write_dependencies).close_task()
+    use_case.execute(todolist_key=todolist_key, task_key=task_key)
+
+def which_task():
+    UseCaseDependencies(adapter_dependencies=read_dependencies).which_task()
+
+
+def task_component(task: TaskSnapshot):
+    def on_task_checkbox_change():
+        close_task(task_key=task.key)
+    st.checkbox(label=task.name, key=task.key, on_change=on_task_checkbox_change)
+
+
+
+def task_list_component():
+    st.header("Mes Tâches")
+    tasks = list_task()
+    if tasks:
+        for task in tasks:
+            task_component(task=task)
+    else:
+        st.write("Aucune tâche pour le moment.")
+
+
 
 
 def main():
     st.set_page_config(page_title="Todo List", page_icon="✅", layout="wide")
 
     add_task_component()
-    st.header("Mes Tâches")
-    tasks = list_task()
-    if tasks:
-        for task in tasks:
-            st.write(f"- {task.name}")
-    else:
-        st.write("Aucune tâche pour le moment.")
+    task_list_component()
+
 
     # # Page de sélection/création de todolist
     # st.title("Mes Todolists")
@@ -106,10 +145,15 @@ def main():
 
 
 def add_task_component():
-    st.header("Ajouter une tâche")
-    task_name = st.text_input("Créer une nouvelle tâche")
-    if st.button("Créer"):
+    def on_click_add_task():
         add_task(task_name=task_name)
+        st.session_state['todolist_new_task'] = ""
+
+    st.header("Ajouter une tâche")
+    task_name = st.text_input("Créer une nouvelle tâche", key="todolist_new_task")
+    st.button("Créer", on_click=on_click_add_task)
+
+
 
 
 if __name__ == "__main__":
